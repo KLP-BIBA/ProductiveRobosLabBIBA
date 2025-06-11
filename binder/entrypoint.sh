55e1e0c9cd871002693dcb60ebc5c0d58c7924fd
konklein <30431645+konklein@users.noreply.github.com>
2025-06-11 14:19:01 +0200

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,7 @@
 source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
-
+roslaunch --wait pr2_gazebo pr2_empty_world.launch &
 # Add other startup programs here
 
 
