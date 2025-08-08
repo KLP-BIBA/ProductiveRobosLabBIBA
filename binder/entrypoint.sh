1846ee63652b915ffc1071c4f3245c0dc77752b3
Yannic Block <blo@biba.uni-bremen.de>
2025-08-08 13:27:20 +0200

    my-first.urdf is shown in rviz with rvizweb now

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Launch the ROS core and web tools when containter starts
 source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch &
-roslaunch --wait pr2_gazebo pr2_empty_world.launch &
-# Add other startup programs here
 
+sleep 5
 
-# The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
+roslaunch rvizweb rvizweb.launch &
+
+# Prepare temporary launch file with arg definition
+MY_LAUNCH=$HOME/work/launch/my-first.launch
+TMP_LAUNCH=/tmp/my-first.launch
+
+cp $MY_LAUNCH $TMP_LAUNCH
+
+# Insert <arg name="urdf_file" default="..." /> if missing
+if ! grep -q '<arg name="urdf_file"' $TMP_LAUNCH; then
+  sed -i '1i\<arg name="urdf_file" default="'"$HOME"'/work/urdf/my-first.urdf" />' $TMP_LAUNCH
+fi
+
+# Launch with argument pointing to your urdf file path
+roslaunch $TMP_LAUNCH urdf_file:=$HOME/work/urdf/my-first.urdf &
+
+# rest of your startup
+
 exec "$@"
+
